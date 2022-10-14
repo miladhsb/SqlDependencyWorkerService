bf0e3092cfab6396c3588e51b928c294fb7a173a
milad khoshkam <milad@gmail.com>
2022-10-14 16:04:28 +0330

    1

--- SqlDependencyWorkerService/Worker.cs
+++ SqlDependencyWorkerService/Worker.cs
@@ -14,6 +14,7 @@ namespace SqlDependencyWorkerService
         }
 
 
+
         //فعال کردن بروکر در دیتابیس برای کتابخانه 
         // ALTER DATABASE[نام دیتابیس] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
         // ALTER DATABASE[نام دیتابیس] SET ENABLE_BROKER;
